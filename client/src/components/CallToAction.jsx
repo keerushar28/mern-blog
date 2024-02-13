1725920ecf6e605f56044dea0f17b89ed9c454a8
Kiran Gautam <gautamkiran878@gmail.com>
2024-02-13 09:43:35 +0545

    add call to action to post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,29 @@
+import { Button } from 'flowbite-react';
+
+export default function CallToAction() {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className="flex-1 justify-center flex flex-col">
+            <h2 className='text-2xl'>
+                Join Us
+            </h2>
+            <p className='text-gray-500 my-2'>
+            
+    🚀  We are coming soon to Google Play Store and the App Store! 
+📲 Stay tuned for an enhanced mobile experience! 🌟 
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <a href="/about" target='_blank' rel='noopener noreferrer'>
+                    Kiran's Blog
+                </a>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+        < img src="https://miro.medium.com/v2/resize:fit:600/format:webp/1*xqT83bMEz92IBYxS9UQNow.png"/>
+        <img src="https://miro.medium.com/v2/resize:fit:600/format:webp/1*nZu0dsnlCQltPT1QMCHFAA.png"/>
+    </div>
+    </div>
+
+
+  )
+}
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -1,6 +1,7 @@
 import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import CallToAction from '../components/CallToAction';
 
 
 export default function PostPage() {
@@ -83,8 +84,9 @@ export default function PostPage() {
         className='p-3 max-w-2xl mx-auto w-full post-content'
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
+
       <div className='max-w-4xl mx-auto w-full'>
-      
+      <CallToAction />
       </div>
       
 
@@ -93,6 +95,7 @@ export default function PostPage() {
         <div className='flex flex-wrap gap-5 mt-5 justify-center'>
           
         </div>
+
       </div>
     </main>
   );
