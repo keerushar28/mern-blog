7ec74a5e713f9839e258b4e2c8b7ea656a64e436
Kiran Gautam <gautamkiran878@gmail.com>
2024-02-15 10:15:36 +0545

    add dashboard overview to admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,25 +1,28 @@
-import React, { useEffect, useState } from 'react';
 import { Sidebar } from 'flowbite-react';
-import { HiUser, HiArrowSmRight, HiDocument, HiDocumentText, HiOutlineUserGroup, HiAnnotation } from 'react-icons/hi';
+import {
+  HiUser,
+  HiArrowSmRight,
+  HiDocumentText,
+  HiOutlineUserGroup,
+  HiAnnotation,
+  HiChartPie,
+} from 'react-icons/hi';
+import { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
 import { signoutSuccess } from '../redux/user/userSlice';
-
+import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 export default function DashSidebar() {
-  const dispatch = useDispatch();
-  const user = useSelector((state) => state.user);
-  const {currentUser} = useSelector(state => state.user);
   const location = useLocation();
-
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector((state) => state.user);
   const [tab, setTab] = useState('');
-
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromURL = urlParams.get('tab');
-
-    if (tabFromURL) {
-      setTab(tabFromURL);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
   }, [location.search]);
   const handleSignout = async () => {
@@ -37,21 +40,31 @@ export default function DashSidebar() {
       console.log(error.message);
     }
   };
-
   return (
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
-        <Sidebar.ItemGroup  className='flex flex-col gap-1'>
-            <Link to='/dashboard?tab=profile'>
-          <Sidebar.Item
-            active={tab === 'profile'}
-            icon={HiUser}
-            label={currentUser.isAdmin ? 'Admin' : 'User'}
-            labelColor="dark"
-            as='div'
-          >
-            Profile
-          </Sidebar.Item>
+        <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
+          <Link to='/dashboard?tab=profile'>
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              labelColor='dark'
+              as='div'
+            >
+              Profile
+            </Sidebar.Item>
           </Link>
           {currentUser.isAdmin && (
             <Link to='/dashboard?tab=posts'>
@@ -64,7 +77,7 @@ export default function DashSidebar() {
               </Sidebar.Item>
             </Link>
           )}
-            {currentUser.isAdmin && (
+          {currentUser.isAdmin && (
             <>
               <Link to='/dashboard?tab=users'>
                 <Sidebar.Item
@@ -86,14 +99,13 @@ export default function DashSidebar() {
               </Link>
             </>
           )}
-          
           <Sidebar.Item
             icon={HiArrowSmRight}
-            className='cursor-pointer'  onClick={handleSignout}
+            className='cursor-pointer'
+            onClick={handleSignout}
           >
             Sign Out
           </Sidebar.Item>
-         
         </Sidebar.ItemGroup>
       </Sidebar.Items>
     </Sidebar>
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
 import DashPosts from '../components/DashPosts';
 import DashUsers from '../components/DashUsers';
 import DashComments from '../components/DashComments';
+import DashboardComp from '../components/DashboardComp';
 
 export default function Dashboard() {
   const location = useLocation();
   const [tab, setTab] = useState('');
-
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFromURL = urlParams.get('tab');
-    
-    if (tabFromURL) {
-      setTab(tabFromURL);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
   }, [location.search]);
-
   return (
-    <div className="min-h-screen flex flex-col md:flex-row">
-      <div className='md:w-56' >
-        {/*  Side Bar */}
+    <div className='min-h-screen flex flex-col md:flex-row'>
+      <div className='md:w-56'>
+        {/* Sidebar */}
         <DashSidebar />
       </div>
-      {/* Profile .... */}
+      {/* profile... */}
       {tab === 'profile' && <DashProfile />}
-      {/* Posts */}
+      {/* posts... */}
       {tab === 'posts' && <DashPosts />}
-        {/* users */}
-        {tab === 'users' && <DashUsers />}
-        {/* Comments */}
-        {tab === 'comments' && <DashComments />}
+      {/* users */}
+      {tab === 'users' && <DashUsers />}
+      {/* comments  */}
+      {tab === 'comments' && <DashComments />}
+      {/* dashboard comp */}
+      {tab === 'dash' && <DashboardComp />}
     </div>
   );
 }
